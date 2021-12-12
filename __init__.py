be837f26f25ff5cc1d47e081a41375cfb2a49281
jarbasai <jarbasai@mailfence.com>
2021-12-12 15:16:29 +0000

    tell me more

--- __init__.py
+++ __init__.py
@@ -10,47 +10,116 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import re
 from os.path import join
 from mycroft.skills.common_query_skill import CommonQuerySkill, CQSMatchLevel
 from mycroft.skills.core import intent_handler
 from neon_solver_wolfram_alpha_plugin import WolframAlphaSolver
+from adapt.intent import IntentBuilder
+from ovos_utils.gui import can_use_gui
+
+
+# TODO move into ovos_utils for reusability
+def remove_parentheses(answer):
+    # remove [xx] (xx) {xx}
+    answer = re.sub(r'\[[^)]*\]', '', answer)
+    answer = re.sub(r'\([^)]*\)', '', answer)
+    answer = re.sub(r'\{[^)]*\}', '', answer)
+    answer = answer.replace("(", "").replace(")", "") \
+        .replace("[", "").replace("]", "").replace("{", "") \
+        .replace("}", "").strip()
+    # remove extra spaces
+    words = [w for w in answer.split(" ") if w.strip()]
+    answer = " ".join(words)
+    if not answer:
+        return None
+    return answer
 
 
 class WolframAlphaSkill(CommonQuerySkill):
-
-    @property
-    def wolfie(self):
-        return WolframAlphaSolver({
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.idx = 0
+        self.last_query = None
+        self.results = []
+        self.wolfie = WolframAlphaSolver({
             "units": self.config_core['system_unit'],
             "appid": self.settings.get("api_key")
         })
+        self.image = None
+        self.skip_images = True  # some wolfram results are pictures with no speech
+                                 # if a gui is available the title is read and image displayed
 
+    # explicit intents
     @intent_handler("search_wolfie.intent")
     def handle_search(self, message):
         query = message.data["query"]
         response = self.ask_the_wolf(query)
         if response:
-            self.speak(response)
-            image = self.wolfie.get_image(query, {"lang": self.lang})
-            self.gui.show_image(image)
+            self.speak_result()
         else:
             self.speak_dialog("no_answer")
 
+    @intent_handler(IntentBuilder("WolfieMore").require("More").
+                    require("WolfieKnows"))
+    def handle_tell_more(self, message):
+        """ Follow up query handler, "tell me more"."""
+        self.speak_result()
+
+    # common query integration
     def CQS_match_query_phrase(self, utt):
         self.log.debug("WolframAlpha query: " + utt)
         response = self.ask_the_wolf(utt)
         if response:
+            self.idx += 1  # spoken by common query framework
             return (utt, CQSMatchLevel.GENERAL, response,
                     {'query': utt, 'answer': response})
 
     def CQS_action(self, phrase, data):
         """ If selected show gui """
-        self.gui["wolfram_image"] = self.wolfie.get_image(phrase, {'lang': self.lang})
+        self.display_wolfie()
+
+    # wolfram integration
+    def ask_the_wolf(self, query):
+        # context for follow up questions
+        self.set_context("WolfieKnows", query)
+        self.results = self.wolfie.long_answer(query)
+        self.idx = 0
+        self.image = None
+        if len(self.results):
+            self.image = self.wolfie.visual_answer(query)
+            return self.results[0]["summary"]
+
+    def display_wolfie(self):
+        image = self.results[self.idx].get("img") or\
+                self.wolfie.visual_answer(self.last_query)
+        # scrollable full result page
+        self.gui["wolfram_image"] = image
         self.gui.show_page(join(self.root_dir, "ui", "wolf.qml"), override_idle=45)
 
-    def ask_the_wolf(self, query):
-        for sentence in self.wolfie.spoken_answers(query):
-            return sentence
+    def speak_result(self):
+        if self.idx + 1 > len(self.results):
+            self.speak_dialog("thats all")
+            self.remove_context("WolfieKnows")
+            self.idx = 0
+        else:
+            if not self.results[self.idx].get("summary"):
+                if not self.skip_images and can_use_gui(self.bus):
+                    self.speak(self.results[self.idx]["title"])
+                    self.display_wolfie()
+                else:
+                    # skip image only result
+                    self.idx += 1
+                    self.speak_result()
+                    return
+            else:
+                self.display_wolfie()
+                # make it more speech friendly
+                ans = self.results[self.idx]["summary"]
+                ans = ans.replace(" | ", "; ").replace("\n", ".\n")
+                ans = remove_parentheses(ans)
+                self.speak(ans)
+            self.idx += 1
 
 
 def create_skill():
--- ui/wolf.qml
+++ ui/wolf.qml
@@ -1,3 +1,4 @@
+import QtQuick.Layouts 1.4
 import QtQuick 2.4
 import QtQuick.Controls 2.0
 import org.kde.kirigami 2.4 as Kirigami
@@ -5,10 +6,20 @@ import Mycroft 1.0 as Mycroft
 
 Mycroft.ScrollableDelegate {
     id: delegateRoot
+    skillBackgroundColorOverlay: "black"
 
-    Image {
-        Layout.fillWidth: true
-        Layout.alignment: Qt.AlignHCenter | Qt.AlignTop
-        source: sessionData.wolfram_image
+    Component.onCompleted: {
+        contentItem.ScrollBar.horizontal.policy = ScrollBar.AlwaysOff
+    }
+
+    Item {
+        width: delegateRoot.width
+        height: wolfImage.implicitHeight
+
+        Image {
+            id: wolfImage
+            anchors.horizontalCenter: parent.horizontalCenter
+            source: sessionData.wolfram_image
+        }
     }
 }
