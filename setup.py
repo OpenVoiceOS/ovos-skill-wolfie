87ce498538310e1089e890406dc8f0855e79ca40
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-09-21 20:14:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 from setuptools import setup
 
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = 'skill-ovos-wolfie.opwnvoiceos=skill_wolfie:WolframAlphaSkill'
+PLUGIN_ENTRY_POINT = 'skill-ovos-wolfie.openvoiceos=skill_wolfie:WolframAlphaSkill'
 
 setup(
     # this is the package name that goes on pip
     name='skill-ovos-wolfie',
     version='0.0.1',
     description='mycroft/ovos wolfram alpha skill plugin',
-    url='https://github.com/JarbasSkills/skill-wolfie',
+    url='https://github.com/OpenVoiceOS/skill-ovos-wolfie',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
-    package_dir={"skill_wolfie": ""},
-    package_data={'skill_wolfie': ['locale/*', 'vocab/*', "dialog/*"]},
-    packages=['skill_wolfie'],
+    package_dir={"skill_ovos_wolfie": ""},
+    package_data={'skill_ovos_wolfie': ['locale/*', 'vocab/*', "dialog/*"]},
+    packages=['skill_ovos_wolfie'],
     include_package_data=True,
     install_requires=["ovos-plugin-manager>=0.0.1a3",
                       "neon-solver-wolfram-alpha-plugin",
