7539fb346913c7036ac50b60465933da2c7ae494
miro <jarbasai@mailfence.com>
2025-03-27 13:17:43 +0000

    feat: fallback handler
    we often get the dreaded "I don't understand, but I'm learning new things every day" dialog.
    
    original mycroft-core directly queried wolfram alpha when no intent matched (before padatious or fallback skills were introduced), it worked surprisingly well for such a simple approach
    
    this commit re-introduces wolfram alpha fallback handler as a last resort

--- __init__.py
+++ __init__.py
@@ -11,268 +11,18 @@
 # limitations under the License.
 #
 
-import tempfile
-from os.path import join, isfile
 from typing import Optional, Tuple
-
-import requests
-from ovos_config import Configuration
 
 from ovos_bus_client import Message
 from ovos_bus_client.session import SessionManager
-from ovos_plugin_manager.templates.language import LanguageTranslator, LanguageDetector
-from ovos_plugin_manager.templates.solvers import QuestionSolver
 from ovos_utils.decorators import classproperty
 from ovos_utils.process_utils import RuntimeRequirements
-from ovos_utils.text_utils import rm_parentheses
-from ovos_workshop.decorators import intent_handler, common_query
-from ovos_workshop.skills.ovos import OVOSSkill
+from ovos_wolfram_alpha_solver import WolframAlphaSolver
+from ovos_workshop.decorators import intent_handler, common_query, fallback_handler
+from ovos_workshop.skills.fallback import FallbackSkill
 
 
-class WolframAlphaApi:
-    def __init__(self, key: str):
-        self.key = key or "Y7R353-9HQAAL8KKA"
-
-    @staticmethod
-    def _get_lat_lon(**kwargs):
-        lat = kwargs.get("latitude") or kwargs.get("lat")
-        lon = kwargs.get("longitude") or kwargs.get("lon") or kwargs.get("lng")
-        if not lat or not lon:
-            cfg = Configuration().get("location", {}).get("coordinate", {})
-            lat = cfg.get("latitude")
-            lon = cfg.get("longitude")
-        return lat, lon
-
-    def spoken(self, query, units="metric", lat_lon=None, optional_params=None):
-        optional_params = optional_params or {}
-        if not lat_lon:
-            lat_lon = self._get_lat_lon(**optional_params)
-        params = {'i': query,
-                  "geolocation": "{},{}".format(*lat_lon),
-                  'units': units,
-                  "appid": self.key,
-                  **optional_params}
-        url = 'https://api.wolframalpha.com/v1/spoken'
-        return requests.get(url, params=params).text
-
-    def simple(self, query, units="metric", lat_lon=None, optional_params=None):
-        optional_params = optional_params or {}
-        if not lat_lon:
-            lat_lon = self._get_lat_lon(**optional_params)
-        params = {'i': query,
-                  "geolocation": "{},{}".format(*lat_lon),
-                  'units': units,
-                  "appid": self.key,
-                  **optional_params}
-        url = 'https://api.wolframalpha.com/v1/simple'
-        return requests.get(url, params=params).text
-
-    def full_results(self, query, units="metric", lat_lon=None, optional_params=None):
-        """Wrapper for the WolframAlpha Full Results v2 API.
-        https://products.wolframalpha.com/api/documentation/
-        Pods of interest
-        - Input interpretation - Wolfram's determination of what is being asked about.
-        - Name - primary name of
-        """
-        optional_params = optional_params or {}
-        if not lat_lon:
-            lat_lon = self._get_lat_lon(**optional_params)
-        params = {'input': query,
-                  "units": units,
-                  "mode": "Default",
-                  "format": "image,plaintext",
-                  "geolocation": "{},{}".format(*lat_lon),
-                  "output": "json",
-                  "appid": self.key,
-                  **optional_params}
-        url = 'https://api.wolframalpha.com/v2/query'
-        data = requests.get(url, params=params)
-        return data.json()
-
-    def get_image(self, query: str, units: Optional[str] = None):
-        """
-        query assured to be in self.default_lang
-        return path/url to a single image to acompany spoken_answer
-        """
-        units = units or Configuration().get("system_unit", "metric")
-        url = 'http://api.wolframalpha.com/v1/simple'
-        params = {"appid": self.key,
-                  "i": query,
-                  # "background": "F5F5F5",
-                  "layout": "labelbar",
-                  "units": units}
-        path = join(tempfile.gettempdir(), query.replace(" ", "_") + ".gif")
-        if not isfile(path):
-            image = requests.get(url, params=params).content
-            with open(path, "wb") as f:
-                f.write(image)
-        return path
-
-
-class WolframAlphaSolver(QuestionSolver):
-    def __init__(self, config=None,
-                 translator: Optional[LanguageTranslator] = None,
-                 detector: Optional[LanguageDetector] = None):
-        super().__init__(config=config, priority=25,
-                         internal_lang="en",
-                         enable_tx=True, enable_cache=False,
-                         translator=translator, detector=detector)
-        self.api = WolframAlphaApi(key=self.config.get("appid") or "Y7R353-9HQAAL8KKA")
-
-    @staticmethod
-    def make_speakable(summary: str):
-        # let's remove unwanted data from parantheses
-        #  - many results have (human: XX unit) ref values, remove them
-        if "(human: " in summary:
-            splits = summary.split("(human: ")
-            for idx, s in enumerate(splits):
-                splits[idx] = ")".join(s.split(")")[1:])
-            summary = " ".join(splits)
-
-        # remove duplicated units in text
-        # TODO probably there's a lot more to add here....
-        replaces = {
-            "cm (centimeters)": "centimeters",
-            "cm³ (cubic centimeters)": "cubic centimeters",
-            "cm² (square centimeters)": "square centimeters",
-            "mm (millimeters)": "millimeters",
-            "mm² (square millimeters)": "square millimeters",
-            "mm³ (cubic millimeters)": "cubic millimeters",
-            "kg (kilograms)": "kilograms",
-            "kHz (kilohertz)": "kilohertz",
-            "ns (nanoseconds)": "nanoseconds",
-            "µs (microseconds)": "microseconds",
-            "m/s (meters per second)": "meters per second",
-            "km/s (kilometers per second)": "kilometers per second",
-            "mi/s (miles per second)": "miles per second",
-            "mph (miles per hour)": "miles per hour",
-            "ª (degrees)": " degrees"
-        }
-        for k, v in replaces.items():
-            summary = summary.replace(k, v)
-
-        # replace units, only if they are individual words
-        units = {
-            "cm": "centimeters",
-            "cm³": "cubic centimeters",
-            "cm²": "square centimeters",
-            "mm": "millimeters",
-            "mm²": "square millimeters",
-            "mm³": "cubic millimeters",
-            "kg": "kilograms",
-            "kHz": "kilohertz",
-            "ns": "nanoseconds",
-            "µs": "microseconds",
-            "m/s": "meters per second",
-            "km/s": "kilometers per second",
-            "mi/s": "miles per second",
-            "mph": "miles per hour"
-        }
-        words = [w if w not in units else units[w]
-                 for w in summary.split(" ")]
-        summary = " ".join(words)
-        return rm_parentheses(summary)
-
-    # data api
-    def get_data(self, query: str,
-                 lang: Optional[str] = None,
-                 units: Optional[str] = None):
-        """
-       query assured to be in self.default_lang
-       return a dict response
-       """
-        units = units or Configuration().get("system_unit", "metric")
-        return self.api.full_results(query, units=units)
-
-    # image api (simple)
-    def get_image(self, query: str,
-                  lang: Optional[str] = None,
-                  units: Optional[str] = None):
-        """
-        query assured to be in self.default_lang
-        return path/url to a single image to acompany spoken_answer
-        """
-        units = units or Configuration().get("system_unit", "metric")
-        return self.api.get_image(query, units=units)
-
-    # spoken answers api (spoken)
-    def get_spoken_answer(self, query: str,
-                          lang: Optional[str] = None,
-                          units: Optional[str] = None):
-        """
-        query assured to be in self.default_lang
-        return a single sentence text response
-        """
-        units = units or Configuration().get("system_unit", "metric")
-        answer = self.api.spoken(query, units=units)
-        bad_answers = ["no spoken result available",
-                       "wolfram alpha did not understand your input"]
-        if answer.lower().strip() in bad_answers:
-            return None
-        return answer
-
-    def get_expanded_answer(self, query,
-                            lang: Optional[str] = None,
-                            units: Optional[str] = None):
-        """
-        query assured to be in self.default_lang
-        return a list of ordered steps to expand the answer, eg, "tell me more"
-
-        {
-            "title": "optional",
-            "summary": "speak this",
-            "img": "optional/path/or/url
-        }
-        """
-        data = self.get_data(query, lang, units)
-        # these are returned in spoken answer or otherwise unwanted
-        skip = ['Input interpretation', 'Interpretation',
-                'Result', 'Value', 'Image']
-        steps = []
-
-        for pod in data['queryresult'].get('pods', []):
-            title = pod["title"]
-            if title in skip:
-                continue
-
-            for sub in pod["subpods"]:
-                subpod = {"title": title}
-                summary = sub["img"]["alt"]
-                subtitle = sub.get("title") or sub["img"]["title"]
-                if subtitle and subtitle != summary:
-                    subpod["title"] = subtitle
-
-                if summary == title:
-                    # it's an image result
-                    subpod["img"] = sub["img"]["src"]
-                elif summary.startswith("(") and summary.endswith(")"):
-                    continue
-                else:
-                    subpod["summary"] = summary
-                steps.append(subpod)
-
-        # do any extra processing here
-        prev = ""
-        for idx, step in enumerate(steps):
-            # merge steps
-            if step["title"] == prev:
-                summary = steps[idx - 1]["summary"] + "\n" + step["summary"]
-                steps[idx]["summary"] = summary
-                steps[idx]["img"] = step.get("img") or steps[idx - 1].get("img")
-                steps[idx - 1] = None
-            elif step.get("summary") and step["title"]:
-                # inject title in speech, eg we do not want wolfram to just read family names without context
-                steps[idx]["summary"] = step["title"] + ".\n" + step["summary"]
-
-            # normalize summary
-            if step.get("summary"):
-                steps[idx]["summary"] = self.make_speakable(steps[idx]["summary"])
-
-            prev = step["title"]
-        return [s for s in steps if s]
-
-
-class WolframAlphaSkill(OVOSSkill):
+class WolframAlphaSkill(FallbackSkill):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.session_results = {}  # session_id: {}
@@ -310,6 +60,22 @@ class WolframAlphaSkill(OVOSSkill):
         else:
             self.speak_dialog("no_answer")
 
+
+    @fallback_handler(priority=80)
+    def handle_wolfram_fallback(self, message):
+        """if this code is reached OVOS is about to give up and
+        speak the "i don't understand" dialog, give wolfram alpha a shot at answering.
+        This is what the original early days mycroft-core did before fallback skills were introduced"""
+        utterance = message.data["utterance"]
+        try:
+            answer = self.ask_the_wolf(utterance, self.lang, self.system_unit)
+            if answer:
+                self.speak(answer)
+                return True
+        except Exception as e:
+            self.log.error(f"Failed to query wolfram alpha: ({e})")
+        return False
+
     # common query integration
     def cq_callback(self, utterance: str, answer: str, lang: str):
         """ If selected show gui """
@@ -320,7 +86,7 @@ class WolframAlphaSkill(OVOSSkill):
         self.gui.show_page("wolf", override_idle=45)
 
     @common_query(callback=cq_callback)
-    def match_common_query(self, phrase: str, lang: str) -> Tuple[str, float]:
+    def match_common_query(self, phrase: str, lang: str) -> Optional[Tuple[str, float]]:
         self.log.debug("WolframAlpha query: " + phrase)
         if self.wolfie is None:
             self.log.error("WolframAlphaSkill not initialized, no response")
@@ -363,14 +129,6 @@ class WolframAlphaSkill(OVOSSkill):
             self.session_results.pop(sess.session_id)
 
 
-WOLFRAMALPHA_PERSONA = {
-  "name": "Wolfram Alpha",
-  "solvers": [
-    "ovos-solver-plugin-wolfram-alpha",
-    "ovos-solver-failure-plugin"
-  ]
-}
-
 if __name__ == "__main__":
     from ovos_utils.fakebus import FakeBus
 
@@ -382,46 +140,3 @@ if __name__ == "__main__":
     print(d.ask_the_wolf("how tall is the eiffel tower", units="nonmetric"))
     # The total height of the Eiffel Tower is 330 meters
     # The total height of the Eiffel Tower is about 1083 feet
-
-    exit()
-
-    query = "who is Isaac Newton"
-
-    # full answer
-    ans = d.spoken_answer(query)
-    print(ans)
-    # Sir Isaac Newton (25 December 1642 – 20 March 1726/27) was an English mathematician, physicist, astronomer, alchemist, theologian, and author (described in his time as a "natural philosopher") widely recognised as one of the greatest mathematicians and physicists of all time and among the most influential scientists.
-
-    ans = d.visual_answer(query)
-    print(ans)
-    # /tmp/who_is_Isaac_Newton.gif
-
-    # chunked answer, "tell me more"
-    for sentence in d.long_answer(query):
-        print("#", sentence["title"])
-        print(sentence.get("summary"), sentence.get("img"))
-
-        # who is Isaac Newton
-        # Sir Isaac Newton was an English mathematician, physicist, astronomer, alchemist, theologian, and author widely recognised as one of the greatest mathematicians and physicists of all time and among the most influential scientists.
-        # https://duckduckgo.com/i/ea7be744.jpg
-
-        # who is Isaac Newton
-        # He was a key figure in the philosophical revolution known as the Enlightenment.
-        # https://duckduckgo.com/i/ea7be744.jpg
-
-        # who is Isaac Newton
-        # His book Philosophiæ Naturalis Principia Mathematica, first published in 1687, established classical mechanics.
-        # https://duckduckgo.com/i/ea7be744.jpg
-
-        # who is Isaac Newton
-        # Newton also made seminal contributions to optics, and shares credit with German mathematician Gottfried Wilhelm Leibniz for developing infinitesimal calculus.
-        # https://duckduckgo.com/i/ea7be744.jpg
-
-        # who is Isaac Newton
-        # In the Principia, Newton formulated the laws of motion and universal gravitation that formed the dominant scientific viewpoint until it was superseded by the theory of relativity.
-        # https://duckduckgo.com/i/ea7be744.jpg
-
-    # bidirectional auto translate by passing lang context
-    sentence = d.spoken_answer("Quem é Isaac Newton",
-                               context={"lang": "pt"})
-    print(sentence)
